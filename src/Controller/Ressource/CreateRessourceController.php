adf543efe3f7583d2def51d3b6815b7be5693218
LLayton <lyes.souifi@gmail.com>
2023-06-23 09:15:49 +0200

    fix catch error

--- src/Controller/Ressource/CreateRessourceController.php
+++ src/Controller/Ressource/CreateRessourceController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Controller\Ressource;
+
+use ApiPlatform\Symfony\Validator\Exception\ValidationException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -9,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Context\Normalizer\ObjectNormalizerContextBuilder;
 use Symfony\Component\Serializer\SerializerInterface;
 use App\Service\AwsS3Service;
+use Symfony\Component\Validator\ConstraintViolationList;
 
 class CreateRessourceController extends AbstractController
 {
@@ -23,32 +26,41 @@ class CreateRessourceController extends AbstractController
     }
 
     public function __invoke(SerializerInterface $serializer,Request $request): Response
-    {
-        $user = $this->security->getUser();
-        $file = $request->files->get('Ressource');
-
-        $context = (new ObjectNormalizerContextBuilder())
-            ->withGroups('User');
-        
-        /*$data = $serializer->serialize(
-            data: $user,
-            format: 'json',
-            context: $context->toArray()
-
-        );*/
-        // Vérifiez si un fichier a été envoyé
-        if (!$file) {
-            return new Response('Aucun fichier n\'a été envoyé.', Response::HTTP_BAD_REQUEST);
+    {   
+        try {
+            $user = $this->security->getUser();
+            $file = $request->files->get('Ressource');
+    
+            $context = (new ObjectNormalizerContextBuilder())
+                ->withGroups('User');
+            
+            /*$data = $serializer->serialize(
+                data: $user,
+                format: 'json',
+                context: $context->toArray()
+    
+            );*/
+            // Vérifiez si un fichier a été envoyé
+            
+            if (!$file) {
+                
+                throw new ValidationException(new ConstraintViolationList(), message: 'Le fichier est vide', code: 400);
+            }
+            
+            $key = './'.$file->getClientOriginalName();
+            $uploadSuccessful =  $this->awsS3Service->uploadFile($key,$file);
+           
+            if ($uploadSuccessful) {
+                return new Response('Le fichier à bien été uploader.', Response::HTTP_CREATED);
+            }else{
+                return new Response("Le fichier n'a pas été uploader.", Response::HTTP_INTERNAL_SERVER_ERROR);
+    
+            }        
+        } 
+            catch (ValidationException $ve) {
+            //throw $th;
+            new Response('Aucun fichier n\'a été envoyé.', Response::HTTP_BAD_REQUEST);
+            }
         }
         
-        $key = './'.$file->getClientOriginalName();
-        $uploadSuccessful =  $this->awsS3Service->uploadFile($key,$file);
-       
-        if ($uploadSuccessful) {
-            return new Response('Le fichier à bien été uploader.', Response::HTTP_CREATED);
-        }else{
-            return new Response("Le fichier n'a pas été uploader.", Response::HTTP_INTERNAL_SERVER_ERROR);
-
-        }
     }
-}
