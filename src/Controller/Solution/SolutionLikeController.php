f8c0da6f21b20e705483a0d2fb6a5f18e6720bd5
Agence-de-Develoloppeurweb <develoloppeurweb@gmail.com>
2023-09-21 01:41:26 +0200

    solutions likes

--- src/Controller/Solution/SolutionLikeController.php
+++ src/Controller/Solution/SolutionLikeController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Solution;
 
 use App\Controller\Like\LikeController;
 use App\Repository\CommentRepository;
+use App\Repository\SolutionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
@@ -14,13 +15,13 @@ class SolutionLikeController extends AbstractController
 {
 
     public function __construct(
-        private readonly CommentRepository $commentRepository,
+        private readonly SolutionRepository $solutionRepository,
         private readonly EntityManagerInterface $em,
         private readonly Security $security
     ){ }
 
     public function __invoke(Request $request, int $id): JsonResponse
     {
-        return (new LikeController($this->em, $this->security))->like($id, $this->commentRepository);
+        return (new LikeController($this->em, $this->security))->like($id, $this->solutionRepository);
     }
 }
